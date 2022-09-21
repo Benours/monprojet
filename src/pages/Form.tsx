c905cd3c2f78d5ab476c66be7e84ea9e11c5d806
x06c <jonathan.miss@outlook.com>
2022-09-21 16:50:50 +0200

    formv0.2

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,25 +1,41 @@
 import { IonButton, IonContent, IonIcon, IonInput, IonItem, IonLabel, IonPage } from "@ionic/react";
 import { addCircle } from "ionicons/icons";
 import { useState } from "react";
+import { Plat } from "../schema/Plat";
 import "./Form.css";
 
 const Form: React.FC = () => {
-    const[form,setForm] = useState();
+    const[form,setForm] = useState<Plat>();
+
+    let name: string;
+    const [ingredient, setIngredient] = useState<string[]>([]);
+    let country: string;
+    let tabloRdm: string[];
+
+    console.log(form);
     return(
         <IonPage>
+            <IonContent>
                 <IonItem>
                     <IonLabel position="floating">Recipe Name</IonLabel>
-                    <IonInput />
+                    <IonInput onIonChange={value => name = value.detail.value!}/>
                 </IonItem>
                 <IonItem>
-                    <IonLabel>Ingredient List</IonLabel>
-                    <IonButton slot="end"><IonIcon icon={addCircle}></IonIcon></IonButton>
+                    <IonLabel position="stacked">Ingredient List</IonLabel>
+                    <IonButton slot="end" onClick={() => {
+                        const ing = [...ingredient,""];
+                        setIngredient(ing);
+                    } }><IonIcon icon={addCircle}>add Ingredient</IonIcon></IonButton>
+                    {ingredient.map((e,i) =>{
+                        return(<IonInput key={i} onIonChange={value => '' }/>)
+                    })}
                 </IonItem>
                 <IonItem>
                     <IonLabel position="floating">Origin Country</IonLabel>
-                    <IonInput />
+                    <IonInput onIonChange={value => country = value.detail.value!}/>
                 </IonItem>
-                <IonButton onClick={()=>{}}>submit</IonButton>
+                </IonContent>
+                <IonButton onClick={()=> setForm({namePlate: name,ingredients: ingredient, nameCity: country})}>submit</IonButton>
         </IonPage>
     );
 }
